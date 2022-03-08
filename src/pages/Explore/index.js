1ac0b3f94f78ed10362974858a832ae12fd081bc
giovannivpgreco <giovannivpgreco@protonmail.com>
2022-03-08 18:47:52 -0300

    Explore screen spec

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 
 export default function Explore() {
+  const history = useHistory();
   return (
-    <Header title="Explore" />
+    <div>
+      <Header title="Explore" />
+      <button
+        type="button"
+        data-testid="explore-foods"
+        onClick={ () => history.push('/explore/foods') }
+      >
+        Explore Foods
+      </button>
+
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => history.push('/explore/drinks') }
+      >
+        Explore Drinks
+      </button>
+    </div>
+
   );
 }
