fa1a3b2540236581f2dcffd3e58ca698243602d3
Ruuybarbosajr <ruuybarbosajr@icloud.com>
2022-03-08 19:02:30 -0300

    colocando inputs e button

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -10,6 +10,31 @@ export default function Search() {
           type="text"
         />
       </label>
+      <label htmlFor="ingredient-search-radio">
+        <input
+          id="ingredient-search-radio"
+          data-testid="ingredient-search-radio"
+          type="radio"
+        />
+        Ingredients
+      </label>
+      <label htmlFor="name-search-radio">
+        <input
+          id="name-search-radio"
+          data-testid="name-search-radio"
+          type="radio"
+        />
+        Name
+      </label>
+      <label htmlFor="first-letter-search-radio">
+        <input
+          id="first-letter-search-radio"
+          data-testid="first-letter-search-radio"
+          type="radio"
+        />
+        First Letter
+      </label>
+      <button type="button" data-testid="exec-search-btn">Search</button>
     </section>
   );
 }
