129a467c9821acbec602a967e7866539babc5a2d
Elania Sousa <elaniaksousa@gmail.com>
2022-03-09 10:55:14 -0300

    Requisito 20

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,27 +1,47 @@
 import React from 'react';
+import drinkIcon from '../../images/drinkIcon.svg';
+import exploreIcon from '../../images/exploreIcon.svg';
+import mealIcon from '../../images/mealIcon.svg';
 
 export default function Footer() {
   return (
     <footer
+      style={ {
+        // left: '0',
+        bottom: '0px',
+        position: 'fixed',
+        // width: '100%',
+        // backgroundColor: 'black',
+        // overflow: 'hidden',
+      } }
       data-testid="footer"
     >
       <button
         type="button"
-        data-testid="drinks-bottom-btn"
       >
-        oi
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="Icondrink"
+        />
       </button>
       <button
         type="button"
-        data-testid="explore-bottom-btn"
       >
-        oi
+        <img
+          data-testid="explore-bottom-btn"
+          src={ exploreIcon }
+          alt="IconExplore"
+        />
       </button>
       <button
         type="button"
-        data-testid="food-bottom-btn"
       >
-        oi
+        <img
+          data-testid="food-bottom-btn"
+          src={ mealIcon }
+          alt="IconMeal"
+        />
       </button>
     </footer>
   );
