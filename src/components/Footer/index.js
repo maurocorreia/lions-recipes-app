2c4de77762e4ef3c9fcfd7af4fc2015537fc43b2
Elania Sousa <elaniaksousa@gmail.com>
2022-03-09 10:32:02 -0300

    Requisito 19

---
+++ src/components/Footer/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer
+      data-testid="footer"
+    >
+      <button
+        type="button"
+        data-testid="drinks-bottom-btn"
+      >
+        oi
+      </button>
+      <button
+        type="button"
+        data-testid="explore-bottom-btn"
+      >
+        oi
+      </button>
+      <button
+        type="button"
+        data-testid="food-bottom-btn"
+      >
+        oi
+      </button>
+    </footer>
+  );
+}
