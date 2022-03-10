0e42a927e92d900a931de8d3ca1bba4b5a6a29e3
giovannivpgreco <giovannivpgreco@protonmail.com>
2022-03-10 03:37:57 -0300

    Progress

--- src/pages/ExploreNationalities/index.js
+++ src/pages/ExploreNationalities/index.js
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import NationCard from '../../components/NationCard';
+import {
+  getNationalities, getFoodsByArea, getAll,
+} from '../../services/ExploreNationalities';
 
 export default function ExploreNationalities() {
+  const [nationalities, setNationalities] = useState([]);
+  const [recipesByNation, setRecipes] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    async function getNations() {
+      setNationalities(await getNationalities());
+    }
+    getNations();
+  }, []);
+
+  async function handleNation(e) {
+    const NATION = e.target.value;
+    if (NATION === 'All') {
+      const DATA = await getAll();
+      console.log(NATION);
+      setRecipes(DATA);
+    } else {
+      const DATA = await getFoodsByArea(NATION);
+      setRecipes(DATA);
+    }
+  }
+
+  async function handleRecipe(e) {
+    const ID = e.target.getAttribute('name');
+    history.push(`/foods/${ID}`);
+  }
+
   return (
     <>
       <Header title="Explore Nationalities" isSearch />
+
+      <select
+        name="nations"
+        id="nations"
+        data-testid="explore-by-nationality-dropdown"
+        onChange={ handleNation }
+      >
+        {nationalities.map((nation, index) => (
+          <option
+            key={ index }
+            value={ nation.strArea }
+            data-testid={ `${nation.strArea}-option` }
+          >
+            { nation.strArea }
+          </option>
+        ))}
+        <option data-testid="All-option" key="all" value="All">All</option>
+      </select>
+
+      {recipesByNation.map((nation, index) => (
+        <button
+          data-testid={ `${index}-recipe-card` }
+          type="button"
+          key={ index }
+          onClick={ handleRecipe }
+        >
+          <NationCard
+            key={ index }
+            recipe={ nation }
+            index={ index }
+          />
+        </button>
+      ))}
+
       <Footer />
     </>
   );
