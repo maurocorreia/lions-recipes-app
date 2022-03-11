451c7f17301a676cbe6292e046fa1b767f9e7274
Mauro Correia <mauroac1@outlook.com>
2022-03-11 16:17:38 -0300

    Fixing.

--- src/components/IngredientsList/index.js
+++ src/components/IngredientsList/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 
 function IngredientsList({ ingredients, measure, idRecipes, type, data }) {
-  console.log(data);
   const history = useHistory();
 
   const [checkedInput, setCheckedInput] = useState(() => {
@@ -66,7 +65,7 @@ function IngredientsList({ ingredients, measure, idRecipes, type, data }) {
       tags: data.strTags ? data.strTags.split(',') : [],
     };
 
-    const prevStore = localStorage.getItem('doneRecipes');
+    const prevStore = JSON.parse(localStorage.getItem('doneRecipes'));
     if (prevStore) {
       localStorage.setItem('doneRecipes', JSON.stringify(
         [...prevStore, doneRecipe],
